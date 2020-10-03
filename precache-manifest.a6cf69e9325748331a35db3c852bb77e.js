1ba13c5ba8a888037d7a3d5b2df938d1fd17e31a
tilankamihingu <tilankamihingu2018@gmail.com>
2020-10-03 03:22:58 -0700

    Updates

---
+++ precache-manifest.a6cf69e9325748331a35db3c852bb77e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9848b276e2d8145e6f5958a4461e540",
+    "url": "/Elearning/index.html"
+  },
+  {
+    "revision": "e75d5a4e43e128119899",
+    "url": "/Elearning/static/css/main.c630d468.chunk.css"
+  },
+  {
+    "revision": "5e9967dc348375d69cbe",
+    "url": "/Elearning/static/js/2.7fac8b4a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Elearning/static/js/2.7fac8b4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e75d5a4e43e128119899",
+    "url": "/Elearning/static/js/main.9469b881.chunk.js"
+  },
+  {
+    "revision": "8ccea8c28e0cb3aeb314",
+    "url": "/Elearning/static/js/runtime-main.2c096d99.js"
+  },
+  {
+    "revision": "468d6fb0760a5ca589aecf3b4404bd90",
+    "url": "/Elearning/static/media/air1.468d6fb0.jpg"
+  },
+  {
+    "revision": "1363afc7a2074d709c3d52cf919fe680",
+    "url": "/Elearning/static/media/air2.1363afc7.jpg"
+  },
+  {
+    "revision": "e76e0c4e1c4f6a823ff92ebb38b15353",
+    "url": "/Elearning/static/media/air3.e76e0c4e.jpg"
+  },
+  {
+    "revision": "204617c92ca4bfece907383c549e5d93",
+    "url": "/Elearning/static/media/def1.204617c9.jpg"
+  },
+  {
+    "revision": "0c3c69e08bf48445185a39267f71cd77",
+    "url": "/Elearning/static/media/def2.0c3c69e0.jpg"
+  },
+  {
+    "revision": "64b50607cba05384048b4cd6a9c2bc6c",
+    "url": "/Elearning/static/media/def3.64b50607.jpg"
+  },
+  {
+    "revision": "bfda25ed01716589fd8f7389a43a8baa",
+    "url": "/Elearning/static/media/four.bfda25ed.jpg"
+  },
+  {
+    "revision": "d9a0d4e9faba1917d0230c345700766d",
+    "url": "/Elearning/static/media/nasa1.d9a0d4e9.png"
+  },
+  {
+    "revision": "958dbdf05e0475513d8061c5bc487811",
+    "url": "/Elearning/static/media/nasa2.958dbdf0.jpg"
+  },
+  {
+    "revision": "75e00025d333f98c9f8937ba80eab876",
+    "url": "/Elearning/static/media/one.75e00025.jpg"
+  },
+  {
+    "revision": "94ecd5ffe42bb91768401f652082f9d8",
+    "url": "/Elearning/static/media/three.94ecd5ff.jpg"
+  },
+  {
+    "revision": "8f0c6e2ed92f6b06acb53e42dfc968f8",
+    "url": "/Elearning/static/media/two.8f0c6e2e.jpg"
+  }
+]);
